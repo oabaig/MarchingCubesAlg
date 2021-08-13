70f332a9e844c3320ff08653da204f9b20eb59cb
Omar Baig <omarbaig1998@gmail.com>
2021-08-13 15:44:01 -0500

    created volume render in cpp

--- Assign3/src/MarchingCubes.cxx
+++ Assign3/src/MarchingCubes.cxx
@@ -1,10 +1,68 @@
 #include "vtkDataSetReader.h"
 #include "vtkSmartPointer.h"
+#include "vtkSmartVolumeMapper.h"
+#include "vtkColorTransferFunction.h"
+#include "vtkPiecewiseFunction.h"
+#include "vtkVolumeProperty.h"
+#include "vtkVolume.h"
+#include "vtkRenderer.h"
+#include "vtkRenderWindow.h"
+#include "vtkRenderWindowInteractor.h"
 
 int main(){
-	vtkSmartPointer<vtkDataSetReader> reader;
-	reader->SetFileName("../../Content/Data/test.vtk");
+	vtkSmartPointer<vtkDataSetReader> reader = vtkSmartPointer<vtkDataSetReader>::New();
+	reader->SetFileName("C:/Dev/UnrealProjects/MarchingCubesAlg/Assign3/src/bernard3D_Q.vtk");
+	reader->Update();
 
+	vtkSmartPointer<vtkSmartVolumeMapper> volumeMapper = vtkSmartPointer<vtkSmartVolumeMapper>::New();
+	volumeMapper->SetInputConnection(reader->GetOutputPort());
+
+	vtkSmartPointer<vtkColorTransferFunction> volumeColor = vtkSmartPointer<vtkColorTransferFunction>::New();
+	volumeColor->AddRGBPoint(-10, 1.0, 1.0, 0.9);
+	volumeColor->AddRGBPoint(-5, 1.0, 1.0, 0.3);
+	volumeColor->AddRGBPoint(5, 1.0, 0.5, 0.3);
+	volumeColor->AddRGBPoint(10, 1.0, 1.0, 0.9);
+
+	vtkSmartPointer<vtkPiecewiseFunction> volumeScalarOpacity = vtkSmartPointer<vtkPiecewiseFunction>::New();
+	volumeScalarOpacity->AddPoint(-10, 0.15);
+	volumeScalarOpacity->AddPoint(-5, 0.15);
+	volumeScalarOpacity->AddPoint(5, 0.85);
+	volumeScalarOpacity->AddPoint(10, 1.0);
+
+	vtkSmartPointer<vtkPiecewiseFunction> volumeGradientOpacity = vtkSmartPointer<vtkPiecewiseFunction>::New();
+	volumeGradientOpacity->AddPoint(0, 0.0);
+	volumeGradientOpacity->AddPoint(90, 0.5);	
+	volumeGradientOpacity->AddPoint(100, 1.0);	
+
+	vtkSmartPointer<vtkVolumeProperty> volumeProperty = vtkSmartPointer<vtkVolumeProperty>::New();
+	volumeProperty->SetColor(volumeColor);
+	volumeProperty->SetScalarOpacity(volumeScalarOpacity);
+	volumeProperty->SetGradientOpacity(volumeGradientOpacity);
+	volumeProperty->SetInterpolationTypeToLinear();
+
+	volumeProperty->ShadeOn();
+	volumeProperty->SetAmbient(0.4);
+	volumeProperty->SetDiffuse(0.6);
+	volumeProperty->SetSpecular(0.2);
+
+	vtkSmartPointer<vtkVolume> volume = vtkSmartPointer<vtkVolume>::New();
+	volume->SetMapper(volumeMapper);
+	volume->SetProperty(volumeProperty);
+
+	vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
+	renderer->SetBackground(0.1, 0.4, 0.2);
+
+	vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
+	renderWindow->SetWindowName("Volume Renderer");
+	renderWindow->AddRenderer(renderer);
+	vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
+	renderWindowInteractor->SetRenderWindow(renderWindow);
+
+	renderer->AddViewProp(volume);
+	renderer->ResetCamera();
+
+	renderWindow->Render();
+	renderWindowInteractor->Start();
 	
 	return 0;
 }
